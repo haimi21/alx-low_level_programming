52d0aaa01c89f1171621e14451c6961270dd0235
haimi21 <haimanotabay84@gmail.com>
2022-12-29 23:27:06 -0800

    asd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: 'for print'
- *
- * Return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	    printf("\"with proper grammar, but the outcome is a piece of art,");
-	    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
